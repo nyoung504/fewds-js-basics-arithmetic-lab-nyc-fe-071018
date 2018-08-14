ad9ca11674e841d9e0c50e11c0a98925a3bf2fb0
Nicole Young <coley504@gmail.com>
2018-08-14 01:06:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random()* 20;
 const randomInteger= Math.floor(randomNumber);
-randomUserID = randomIntegerMath.floor(randomInteger);
+randomUserID = randomInteger + 1000000001;
